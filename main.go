80a915f02404d20969def3f4ceaa9d58ef5378f8
itskris <kkangro7@gmail.com>
2021-11-30 10:26:42 +0200

    register form

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+var userInfo User
 
 type User struct {
 	email    string
@@ -21,6 +25,28 @@ func checkErr(err error) {
 }
 
 func registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		r.ParseForm()
+
+		userInfo = User{
+			email:    r.FormValue("email"),
+			username: r.FormValue("username"),
+			password: r.FormValue("password"),
+		}
+
+		db, err := sql.Open("sqlite3", "./db/names.db")
+		checkErr(err)
+
+		defer db.Close()
+
+		stmt, err := db.Prepare("INSERT INTO user (username, password, email) VALUES (?, ?, ?)")
+		checkErr(err)
+
+		fmt.Println("Userinfo", userInfo)
+		stmt.Exec(userInfo.username, userInfo.password, userInfo.email)
+		defer stmt.Close()
+	}
+
 	tmpl, err := template.ParseFiles("./templates/register.html")
 	if err != nil {
 		http.Error(w, "Error occured during parsing template", 500)
@@ -28,22 +54,17 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Error occured during the execution of template", 500)
+		log.Fatal(err)
+	}
 }
 
 func main() {
-	/* db, err := sql.Open("sqlite3", "names.db")
-	checkErr(err)
-
-	defer db.Close()
-
-	stmt, err := db.Prepare("INSERT INTO people (first_name, last_name) VALUES (?, ?)")
-	checkErr(err)
-
-	stmt.Exec(person.first_name, person.last_name)
-	defer stmt.Close() */
-
 	http.HandleFunc("/", registerHandler)
+
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 	}
+
 }
