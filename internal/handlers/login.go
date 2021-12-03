2e24e045410b206d0da853f34c54c840bd97c188
itskris <kkangro7@gmail.com>
2021-12-03 23:12:48 +0200

    register feedback

--- internal/handlers/login.go
+++ internal/handlers/login.go
@@ -16,9 +16,9 @@ var LoginMsgs LoginMessages
 
 func Login(w http.ResponseWriter, r *http.Request) {
 
-	/* if RegMsgs.Succesful { // If registration was succesful
+	if RegMsgs.Succesful { // If registration was succesful
 		LoginMsgs.SuccesfulRegister = true
-	} */
+	}
 	tpl, _ := template.ParseFiles("./templates/login.html")
 	err := tpl.Execute(w, LoginMsgs) // LoginMsgs is created in Login
 	if err != nil {
@@ -27,5 +27,6 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "500 Internal Server error", 500)
 		return
 	}
+	RegMsgs.Succesful = false   // Reset the registration message after we have been redirected to login page
 	LoginMsgs = LoginMessages{} // Reset the login messages or they wont change upon reloading the page
 }
