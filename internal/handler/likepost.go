9d2f6348374d89672244da35c2ca4052221250ae
itskris <kkangro7@gmail.com>
2021-12-14 13:14:28 +0200

    documentation for likepost.go

--- internal/handler/likepost.go
+++ internal/handler/likepost.go
@@ -40,42 +40,52 @@ func LikePost(env *env.Env) http.HandlerFunc {
 			return
 		}
 
-		fmt.Println(postid)
-		fmt.Println(isLike)
-		// Check if user has liked or disliked
-		// SELECT like FROM postlikes WHERE userid = ?
-		// If like != islike
-		// update..
-
-		row := db.QueryRow("SELECT like FROM postlikes WHERE userid = ? AND postid = ?", userid, postid)
-
-		var like int
-		switch err := row.Scan(&like); err {
-
-		case sql.ErrNoRows:
-			fmt.Println("Here")
-			stmt, err := db.Prepare("INSERT INTO postlikes (userid, postid, like) VALUES (?, ?, ?)")
-			if err != nil {
-				http.Error(w, err.Error(), 500)
-				return
-			}
-			stmt.Exec(userid, postid, isLike)
-
-		case nil:
-			if like != isLike {
-				stmt, err := db.Prepare("UPDATE postlikes SET like = ? WHERE userid = ?")
-				if err != nil {
-					http.Error(w, err.Error(), 500)
-					return
-				}
-				fmt.Println(err)
-				stmt.Exec(isLike, userid)
-			}
-
-		default:
+		err = checkUserLikes(db, isLike, postid, userid)
+		if err != nil {
 			http.Error(w, err.Error(), 500)
 			return
 		}
 
 	}
+}
+
+/*
+1. Looks if user has reacted to that post
+
+2. If it founds user has reacted
+	2.1 If user previously liked that post and then dislikes it, the "like" value is updated accordingly
+	2.2 If user previously disliked that post and then likes it, the "like" value is updated accordingly
+
+3. If it founds user has not reacted
+	3.1 Add the postid, userid and like(0 if disliked, 1 if liked) to "postlikes" table
+*/
+func checkUserLikes(db *sql.DB, isLike int, postid string, userid int) error {
+	// Query for which returns us whether user has liked or disliked that post
+	row := db.QueryRow("SELECT like FROM postlikes WHERE userid = ? AND postid = ?", userid, postid)
+
+	var like int
+	switch err := row.Scan(&like); err {
+
+	case sql.ErrNoRows: // If user doesnt have a like or dislike for that post
+		stmt, err := db.Prepare("INSERT INTO postlikes (userid, postid, like) VALUES (?, ?, ?)")
+		if err != nil {
+			return err
+		}
+		stmt.Exec(userid, postid, isLike)
+
+	case nil: // If user has liked or disliked the post, we check
+		if like != isLike {
+			stmt, err := db.Prepare("UPDATE postlikes SET like = ? WHERE userid = ?")
+			if err != nil {
+				return err
+			}
+			fmt.Println(err)
+			stmt.Exec(isLike, userid)
+		}
+
+	default:
+		return err
+	}
+
+	return nil
 }
