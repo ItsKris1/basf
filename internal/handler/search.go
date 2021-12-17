56c24405f49bcc2eb859723a4af6e56465dbad32
itskris <kkangro7@gmail.com>
2021-12-17 10:48:48 +0200

    search for posts at homepage

--- internal/handler/search.go
+++ internal/handler/search.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"forum/internal/env"
 	"forum/internal/session"
 	"forum/internal/tpl"
@@ -21,11 +20,49 @@ func Search(env *env.Env) http.HandlerFunc {
 		}
 
 		tagName := r.URL.Query().Get("tags")
-		fmt.Println(tagName)
+
+		db := env.DB
+
+		var tagid string
+		if err := db.QueryRow("SELECT id FROM tags WHERE name = ?", tagName).Scan(&tagid); err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		rows, err := db.Query("SELECT postid FROM posttags WHERE tagid = ?", tagid)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		var results []Post
+		for rows.Next() {
+			var post Post
+
+			var postid string
+			if err := rows.Scan(&postid); err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+
+			if err := db.QueryRow("SELECT title, body, creation_date FROM posts WHERE postid = ?", postid).Scan(&post.Title, &post.Body, &post.CreationDate); err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+
+			results = append(results, post)
+		}
+
+		if err := rows.Err(); err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
 
 		searchPage := SearchResultsPage{
 			UserInfo: session.UserInfo,
+			Results:  results,
 		}
+
 		tpl.RenderTemplates(w, "searchresults.html", searchPage, "./templates/base.html", "./templates/searchresults.html")
 		return
 	}
